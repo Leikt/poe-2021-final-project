ae1f8b64d8849a3e7ee410961c1546a2e578337a
leikt <leikt.solreihin@gmail.com>
2021-11-14 15:06:01 +0100

    add connection script

---
+++ sources/setup_connection_to_master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+key_name=.ssh/aws_poe_pf
+host=aws_poe_pf
+port=22
+user=sysadmin
+hostname=ec2-18-231-196-41.sa-east-1.compute.amazonaws.com
+
+# Create ssh key if it does not exist
+ssh-keygen -t rsa -N '' -f $key_name
+
+# Add key to ssh agent
+eval `ssh-agent -s`
+ssh-add $key_name
+
+# Configure comfy ssh
+echo -e "\nHost $host\n\tHostname $hostname\n\tUser $user\n\tIdentityFile$key_name\n\tPort=$port" >> .ssh/config
+
+# Send the public key to the server
+key_content=`cat $key_name.pub`
+ssh $host "echo $key_content >> .ssh/authorized_keys"
